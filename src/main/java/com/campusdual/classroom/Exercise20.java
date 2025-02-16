32d47ec9dbd7f6bf022f0e135ab2b7da4dc09e4e
Jorge González Fernández <j.gonzalezf@udc.es>
2025-02-16 19:32:39 +0100

    Colecciones de tipo lista

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,16 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
-
+        List<Person> person = new ArrayList<>();
+        person.add(new Person("John", "Smith"));
+        person.add(new Teacher("María", "Montessori", "Educación"));
+        person.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        person.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return person;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        stringList.forEach(Person::getDetails);
     }
 
     public static void main(String[] args) {
